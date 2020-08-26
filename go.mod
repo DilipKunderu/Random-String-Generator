d9957e9ef93ae25ed6d4d572480fb39bb91b9a49
DilipKunderu <dilipkunderu@gmail.com>
2020-08-25 23:59:34 -0700

    tested String generation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DilipKunderu/Random-String-Generator
+
+go 1.14
---
+++ rand/strings_test.go
@@ -0,0 +1,7 @@
+package rand
+
+import "testing"
+
+func TestThatStringIsGenerated(t *testing.T){
+	print(String(20))
+}
